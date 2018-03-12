d95234d171ea936825414a2e799566c463d2ad7f
diego molina cote <netcote1@hotmail.com>
2018-03-11 20:52:04 -0500

    Entrenamiento

---
+++ business/Login.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace business
+{
+    public class Login
+    {
+        public int ValidarLogin(int entero)
+        {
+            int response = 0;
+
+            return response;
+        }
+    }
+}
